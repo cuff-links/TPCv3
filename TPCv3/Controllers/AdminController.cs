f879616e65fd6670bfa060f37f1d72132095d940
Silne Dorlus <silne30@hotmail.com>
2013-04-12 21:14:51 -0400

    Added membership provider, login Action and login view.
    Signed-off-by: Silne Dorlus <silne30@hotmail.com>

--- TPCv3/Controllers/AdminController.cs
+++ TPCv3/Controllers/AdminController.cs
@@ -1,4 +1,7 @@
 ﻿using System.Web.Mvc;
+using System.Web.Security;
+
+using TPCv3.Models;
 
 namespace TPCv3.Controllers{
     [Authorize]
@@ -19,6 +22,30 @@ namespace TPCv3.Controllers{
 
             this.ViewBag.ReturnUrl = returnUrl;
             return this.View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model, string returnUrl){
+            if (this.ModelState.IsValid){
+                if (Membership.ValidateUser(model.Username, model.Password)){
+                    FormsAuthentication.SetAuthCookie(model.Username, false);
+
+                    if (!string.IsNullOrEmpty(returnUrl) && this.Url.IsLocalUrl(returnUrl)){
+                        return this.Redirect(returnUrl);
+                    }
+                    return this.RedirectToAction("Manage");
+                }
+                this.ModelState.AddModelError("", "The username or password provided is incorrect.");
+            }
+            return this.View();
+        }
+
+        public ActionResult Logout(){
+            FormsAuthentication.SignOut();
+            this.Session.Clear();
+            return this.RedirectToAction("Login", "Admin");
         }
 
         #endregion
